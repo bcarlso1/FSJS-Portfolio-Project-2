303c8bce94c9f29dd4b20b88dda8af1940316ec8
bcarlso1 <bcarlso1@gmail.com>
2020-01-25 15:16:05 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,157 +1,50 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const items = document.querySelectorAll('.student-item');
 
 let numberPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+/***************** 
+PAGE DISPLAY FUNCTION
+********************/
 
 const list = document.querySelectorAll(".student-item");
 
 function pageDisplay(page) {
+   var start = (page*10)-10;
+   var end = page*10;
    for (var i = 0; i < list.length; i++) {
-       if (page == 1) {
-         if (i <= 9) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
-      }
-
-      if (page == 2) {
-         if (i <= 19  && i > 9) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
-      }
-      
-      if (page == 3) {
-         if (i <= 29 && i > 19) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
-      }
-      
-      if (page == 4) {
-         if (i <= 39 && i > 29) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
-      }
-
-      if (page == 5) {
-         if (i <= 49 && i > 39) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
-      }
-
-      if (page == 6) {
-         if (i <= 59 && i > 49) {
-            list[i].style.display = "block";
-         } else {
-            list[i].style.display = "none";
-         }
+       if (i >= start && i < end) {
+         list[i].style.display = "block";
+       } else {
+         list[i].style.display = "none";
       }
    }
 
    }
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-function appendPageLinks() {
+/*********************** 
+  APPEND PAGE FUNCTION
+**********************/
+function appendPageLinks(list1) {
    pageDisplay(1);
    pageButton = document.createElement('div');
    pageButton.classList.add('pagination');
    newUL = document.createElement('ul');
    pageButton.appendChild(newUL);
-   document.getElementById('list').insertAdjacentElement('afterend', pageButton)
-   function createComponents() {
+   document.getElementById('list').insertAdjacentElement('afterend', pageButton);
+
+   const studentList = document.querySelectorAll(".student-item");
+   let count = 1;
+   for (var i = 0; i < studentList.length; i++) {
+         if ((i-1) % 10 == 0) {
             pageButton = document.createElement('li');
             pageButtonLink = document.createElement('a');
-   }
-   function addComponent() {
-      pageButton.appendChild(pageButtonLink);
-      document.getElementsByClassName('pagination')[0].firstElementChild.appendChild(pageButton);
-   }
-   for (var i = 0; i < list.length; i++) {
-         if (i == 1) {
-            createComponents();
-            pageButton1Text = document.createTextNode('1');
+            pageButton1Text = document.createTextNode(count);
             pageButtonLink.appendChild(pageButton1Text);
-            addComponent();
-         }
-         else if (i == 11) {
-            createComponents();
-            pageButton2Text = document.createTextNode('2');
-            pageButtonLink.appendChild(pageButton2Text);
-            addComponent();
-         }
-         else if (i == 21) {
-            createComponents();
-            pageButton3 = document.createElement('li');
-            pageButton3Text = document.createTextNode('3')
-            pageButtonLink.appendChild(pageButton3Text);
-            addComponent();
-         }
-         else if (i == 31) {
-            createComponents();
-            pageButton4 = document.createElement('li');
-            pageButton4Text = document.createTextNode('4');
-            pageButtonLink.appendChild(pageButton4Text);
-            addComponent();
-         }
-         else if (i == 41) {
-            createComponents();
-            pageButton5Text = document.createTextNode('5')
-            pageButtonLink.appendChild(pageButton5Text);
-            addComponent();
-         }
-         else if (i == 51) {
-            createComponents();
-            pageButton6Text = document.createTextNode('6')
-            pageButtonLink.appendChild(pageButton6Text);
-            addComponent();
-         }
-         else {
-            console.log('sup');
+            pageButton.appendChild(pageButtonLink);
+            document.getElementsByClassName('pagination')[0].firstElementChild.appendChild(pageButton);
+            count++;
+            console.log(i);
          }
    }
 
@@ -159,6 +52,10 @@ function appendPageLinks() {
 
 
 window.onload = appendPageLinks();
+
+/* ******************
+Remove Active Class Fxn for Event Listener
+******************** */
 
 function removeActive() {
    var numberedLinks = document.getElementsByTagName('a');
@@ -170,6 +67,9 @@ function removeActive() {
 /* ******************
 EVENT LISTENER FOR PAGES
 ******************** */
+
+// NEED to make this dynamic ***************
+
 
 document.getElementsByTagName('a')[0].addEventListener("click", (e) => {
    pageDisplay(1);
@@ -217,31 +117,51 @@ var pageHeader = document.getElementsByClassName('page-header')[0]
 var searchDiv = document.createElement('div');
 searchDiv.classList.add('student-search');
 var searchInput = document.createElement('input');
-//.setAttribute("type", "search");
-searchInput.setAttribute('plaeholder', 'Search for students...');
+searchInput.setAttribute("placeholder", "Search for students...");
 searchDiv.appendChild(searchInput);
+var searchButton = document.createElement('button');
+searchButton.innerHTML = "Search";
+searchDiv.appendChild(searchButton);
 pageHeader.appendChild(searchDiv);
- // <input placeholder="Search for students...">
-  //<button>Search</button>
 
-// Create two variables to store the start index and the end index of 
-/* the list items to be displayed on the given page. To make this function
- dynamic and work with a list of any length, a bit of basic math can be
-  used to determine these values.
-Start Index = (page parameter * items per page) - items per page
-End Index = page parameter * items per page
-Loop over the list parameter.
-Inside the loop, display any list item with an index that is greater 
-than or equal to the start index variable and less than the end index variable.
+ 
+/* **********
+AUTOCOMPLETE SEARCH
+************** */
+
+const searchBar = document.getElementsByTagName("input")[0];
+var students = document.getElementsByClassName('student-item');
+var searchClick = document.getElementsByTagName('button')[0];
+
+searchClick.addEventListener('onclick', myFunction);
+searchBar.addEventListener('keyup', myFunction);
+
+function myFunction() {
+    searchBar.value = searchBar.value.toLowerCase();
+    var mySearch = searchBar.value;
+    console.log(mySearch);
+    // var tally = 0;
+    for (var i = 0; i < students.length; i += 1) {
+        var title = students[i].innerHTML.toLowerCase();
+        console.log(title);
+        if (title.includes(mySearch)) {
+       //  tally++;
+         students[i].style.display = "";
+         } else {
+         students[i].style.display = "none";
+          }      
+         // if (tally == 0) {
+        //     alert('no results');
+      //    }
+}
+}
+;
 
 
-Pro Tip: You can divide list.length by the max number of items per page to f
-igure out how many pages are needed, and you can use a loop that iterates 
-that many times to create the correct number of LI elements.
+
+
+/*Pro Tip: 
 Each LI element should contain an A element with an href attribute of #, 
 and text set to the page number each link will show. First link is 1. 
-Second link is 2. And so on.
-*/
+Second link is 2. And so on.*/
 
-// Remember to delete the comments that came with this file, and replace them 
-//with your own code comments.
