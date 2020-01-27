3c582c666ecb43d9f2030f77cb4f7db7cc5fd5d5
bcarlso1 <bcarlso1@gmail.com>
2020-01-26 22:53:05 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ PAGE DISPLAY FUNCTION
 
 const defaultList = document.querySelectorAll(".student-item");
 
+// functions asks for what page you want to show and for the list of students
 function pageDisplay(page, list) {
+ // set parameters for which students to show on each page
    var start = (page*10)-10;
    var end = page*10;
    for (var i = 0; i < list.length; i++) {
        if (i >= start && i < end) {
+          // display students within parameters
          list[i].style.display = "block";
        } else {
          list[i].style.display = "none";
@@ -25,17 +28,25 @@ function pageDisplay(page, list) {
 /*********************** 
   APPEND PAGE FUNCTION
 **********************/
-function appendPageLinks(page, list) {
-   pageDisplay(page, list);
+function appendPageLinks(list) {
+   // display page 1
+   pageDisplay(1, list);
+   // clear out any prior append
+   if (document.body.contains(document.getElementsByClassName('pagination')[0])) {
+      var pageDiv = document.getElementsByClassName('page')[0];
+      pageDiv.removeChild(pageDiv.childNodes[4]);
+   }
+
+   // put together elements for page buttons
    pageButton = document.createElement('div');
    pageButton.classList.add('pagination');
    newUL = document.createElement('ul');
    pageButton.appendChild(newUL);
    document.getElementById('list').insertAdjacentElement('afterend', pageButton);
 
-   const studentList = document.querySelectorAll(".student-item");
+   // set up the number of page links dependent on the list lenth
    let count = 1;
-   for (var i = 0; i < studentList.length; i++) {
+   for (var i = 0; i < list.length; i++) {
          if ((i-1) % 10 == 0) {
             pageButton = document.createElement('li');
             pageButtonLink = document.createElement('a');
@@ -47,11 +58,13 @@ function appendPageLinks(page, list) {
           //  console.log(i);
          }
    }
+   eventListen(list);
 
 }
 
-
-window.onload = appendPageLinks(1, defaultList);
+// run fxn when page first loads - set up buttons, show p.1 content
+const studentList = document.querySelectorAll(".student-item");
+window.onload = appendPageLinks(studentList);
 
 /* ******************
 Remove Active Class Fxn for Event Listener
@@ -67,18 +80,19 @@ function removeActive() {
 /* ******************
 EVENT LISTENER FOR PAGES
 ******************** */
-
+function eventListen(list) {
 var numLinks = document.getElementsByTagName('a');
 
 for (var i = 0; i < numLinks.length; i++) {
 let counter = i+1;
 document.getElementsByTagName('a')[i].addEventListener("click", (e) => {
-   pageDisplay(counter, defaultList);
+   pageDisplay(counter, list);
    removeActive();
    event.target.classList.add("active");
 }
-);
+)
 }
+};
 
 
 /* *************************
@@ -122,17 +136,30 @@ function myFunction() {
          foundStudentsArray.push(studentsContainer[i]);      
          }
    }
-   for (var i = 0; i < students.length; i += 1) {
+   for (var i = 0; i < foundStudentsArray.length; i += 1) {
      foundStudentsArray[i].style.display = "block";
    } 
 // fix here to display results properly on different pages
+// Pagination links display based on how many search results are returned. For example: if 10 or fewer results are returned, 0 or 1 pagination links are displayed. If 22 search results are returned, 3 pagination links are displayed.
+// add few more code comments explaining
 // *************************
-
-   if (foundStudentsArray.length == 0) {
-      alert('hey');
+   var noResultsNow = document.getElementById('no-results');
+  // if no results but not first time
+   if (foundStudentsArray.length == 0 && document.body.contains(noResultsNow)) {
+      noResultsNow.innerHTML = "No results";  
+   // if no results but IS first time
+   } else if (foundStudentsArray.length == 0 ) {
       var noResults = document.createElement('div');
       noResults.innerHTML = "No results";
+      noResults.setAttribute("id", "no-results");
       document.getElementById('list').appendChild(noResults);
+   // if array is NOT 0
+   } else {
+   // if array is not 0 but it USED TO BE
+      if (document.body.contains(noResultsNow)) {
+      noResultsNow.innerHTML = "";
+      }
    }
+ appendPageLinks(foundStudentsArray);
 };
 
